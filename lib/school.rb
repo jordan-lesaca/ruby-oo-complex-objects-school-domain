25a68b089331517348ddf68365c317a97c44ed5f
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-03-11 13:51:10 -0800

    commit tree 741703c7c0b75c3b8c1a4a58b56f1d7c45dca5a5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+
+    # attr_accessor 
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(stud_name, grade)
+        roster[grade] ||= []
+        roster[grade] << stud_name
+    end       
+
+    def grade(stud_grade)
+        roster[stud_grade]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, names|
+            sorted[grade] = names.sort
+        end
+        sorted
+
+    end
+
+
+end
